145e7eb3346327378a812d9d7a00370fc8654d00
thedarkside95 <iliasrafael@gmail.com>
2016-10-16 17:32:32 +0300

    Structs

--- Structs.h
+++ Structs.h
@@ -1,1 +1,16 @@
 /* domes */
+
+typedef struct list_node {
+	int neighbor[N];
+	int edgeProperty[N];
+	int offset;	
+}list_node;
+
+typedef struct NodeIndex {
+	list_node* list_head;
+}NodeIndex;
+
+NodeIndex* createNodeIndex();
+int InsertNode(NodeIndex* index, int id);
+list_node* getListHead(NodeIndex* index, int id);
+int destroyNodeIndex(NodeIndex* index);
