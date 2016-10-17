badc0da740ddfb6199ae59942dc1e0554bdfc18b
span1495 <span95@outlook.com.gr>
2016-10-17 14:10:52 +0300

    merikes prostthike-exei lathakia

--- Structs.h
+++ Structs.h
@@ -1,18 +1,32 @@
 /* domes */
+#include <stdint.h>
 #define N 100
 
+bool OK_SUCCESS;
+
 class list_node {
-	int neighbor[N];
-	int edgeProperty[N];
+	uint32_t neighbor[N];
+	uint32_t edgeProperty[N];
 	int offset;	
 };
 
 class NodeIndex {
 	list_node* list_head;
+
+	NodeIndex* createNodeIndex();
+	int InsertNode(NodeIndex* index, int id);
+	list_node* getListHead(NodeIndex* index, int id);
+	int destroyNodeIndex(NodeIndex* index);
 };
 
 class Buffer{
 	list_node *cells;
+
+
+	Buffer* createBuffer();
+	int allocNewNode(Buffer*);
+	list_node* getListNode(int);
+	OK_SUCCESS destroyBuffer(Buffer*);
 };
 
 class Graph{
@@ -21,8 +35,9 @@ class Graph{
 	Buffer * inc_buffer;
 	Buffer * out_buffer;
 };
-
-NodeIndex* createNodeIndex();
-int InsertNode(NodeIndex* index, int id);
-list_node* getListHead(NodeIndex* index, int id);
-int destroyNodeIndex(NodeIndex* index);
+/*
+Graph::Graph()
+{
+	
+}
+*/
