60ce490e6cf413b2296dc5c19aa3fe7884192260
span1495 <span95@outlook.com.gr>
2016-10-19 18:19:35 +0300

    diavasma arxeiou
    a

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include "Structs.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
 
 int main(void)
 {
 	Graph graph;
+
+ ifstream myReadFile;
+ myReadFile.open("tinyGraph.txt");
+ unsigned int node;
+ unsigned int edge;
+ if (myReadFile.is_open()) {
+ 	while (!myReadFile.eof()) {
+    	myReadFile >> node >>edge;
+    	if(myReadFile.good())
+    		cout<<node<<" "<<edge<<endl;
+    	else break;
+    }
+}
+myReadFile.close();
 	return 0;
 }
